ba1f3436fec0e3d113b982d02f13bec51719fe9b
Artem Bezzubets <artem.bezzubets@gmail.com>
2015-05-14 18:00:50 -0400

    Multitasking, Parameters from CSV and Logging
    Added support to work with several servers from one console.
    Input parameters now is parsing from .csv file.
    Added logging.

--- EGen1/EGen1/Program.cs
+++ EGen1/EGen1/Program.cs
@@ -7,23 +7,40 @@ using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
 
 namespace ExchangeChangeGenerator
 {
     class Program
     {
+        private static readonly object _lock = new object();
+
         static void Main(string[] args)
         {
-            string serverIP = args[0];
-            string[] credentials = new string[] {args[1],args[2]};
-            int amountToGenerate = Convert.ToInt32(args[3]);
-            int generateTime = Convert.ToInt32(args[4]);
-            int messageSize = Convert.ToInt32(args[5]);
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please specify path to .csv file with server parameters.\nExample: ExchangeChGen.exe \"C:\\User\\Administrator\\Desktop\\servers.csv\"");
+            }
+            else
+            {
+                Program.writeToLog("---------------------------------------------------------");
+                ServicePointManager.ServerCertificateValidationCallback = CertificateValidationCallBack;
 
-            // Certificate validation
-            ServicePointManager.ServerCertificateValidationCallback = CertificateValidationCallBack;
-            ExchangeGenerator server1 = new ExchangeGenerator(serverIP, credentials, amountToGenerate, generateTime, messageSize);
-            server1.startGenerator();
+                List<ExchangeGenerator> serverList = new List<ExchangeGenerator>();
+
+                TextFieldParser parser = new TextFieldParser(args[0]);
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                while (!parser.EndOfData)
+                {
+                    string[] fields = parser.ReadFields();
+                    serverList.Add(new ExchangeGenerator(fields[0], new string[] { fields[1], fields[2] },
+                        Convert.ToInt32(fields[3]), Convert.ToInt32(fields[4]), Convert.ToInt32(fields[5])));
+
+                }
+
+                Parallel.ForEach(serverList, s => s.startGenerator());
+            }
         }
 
         // Forced certificate validation to return always true
@@ -34,6 +51,28 @@ namespace ExchangeChangeGenerator
         System.Net.Security.SslPolicyErrors sslPolicyErrors)
         {
             return true;
+        }
+
+        public static void writeToLog(string logMessage, string serverIP)
+        {
+            lock (_lock)
+            {
+                using (StreamWriter w = File.AppendText("log.txt"))
+                {
+                    w.WriteLine("[{0} - {1}] - {2}", serverIP, DateTime.Now.ToString(), logMessage);
+                }
+            }
+        }
+
+        public static void writeToLog(string logMessage)
+        {
+            lock (_lock)
+            {
+                using (StreamWriter w = File.AppendText("log.txt"))
+                {
+                    w.WriteLine("[{0}] - {1}", DateTime.Now.ToString(), logMessage);
+                }
+            }
         }
     }
 }
